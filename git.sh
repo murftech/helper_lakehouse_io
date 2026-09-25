bf6f0de8504b7a942bf8d009ba929b5418a4a8c5
murftech <murftech7@gmail.com>
2026-09-25 21:41:42 +0800

    rename io_helpers -> lakehouse_io

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 git init
 git add .
 git commit -m "initial commit: io_helpers package (pyarrow/pyiceberg/sparkiceberg/sparkdelta/deltalake write helpers + shared schema guards)"
-gh repo create helper_io_helpers --public --source=. --remote=origin --push
+gh repo create helper_lakehouse_io --public --source=. --remote=origin --push
 
 git remote -v
 gh repo view --web
